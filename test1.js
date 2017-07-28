2452fa0aa1fa08ffa5cd54ef0ead9620a5a42da9
josh <joshua@marinacci.org>
2017-07-27 22:01:20 -0700

    tweaks

--- test1.js
+++ test1.js
@@ -19,7 +19,41 @@ function lerps(t, values) {
     return lerp(fract,values[band],values[band+1]);
 }
 
+function lerpRGB(t, A, B) {
+    return {
+        r: lerp(t, A.r, B.r),
+        g: lerp(t, A.g, B.g),
+        b: lerp(t, A.b, B.b)
+    }
+}
 
+function lerpRGBs(t, arr) {
+    return {
+        r: lerps(t, arr.map((C)=>C.r)),
+        g: lerps(t, arr.map((C)=>C.g)),
+        b: lerps(t, arr.map((C)=>C.b)),
+    }
+}
+
+function clampToBands(v,bands) {
+    return Math.round(v*bands)/bands;
+}
+
+/*
+    calls the function cb on every element of a 2d array, passing in the x and y values,
+    plus tx and ty, which go from 0 to 1 across the width and height, centered on the pixels.
+ */
+function gen_xy(width, height, cb) {
+    let data = [];
+    for(let x=0; x<width; x++) {
+        for (let y = 0; y < height; y++) {
+            const i = x+y*width;
+            const nx = x/width - 0.5, ny = y/height - 0.5;
+            data[i] = cb(x,y,nx,ny);
+        }
+    }
+    return data;
+}
 
 function makeColorHSV(h,s,v) {
 }
@@ -46,34 +80,6 @@ function randArray(length) {
     return out;
 }
 
-const min = Math.min;
-
-/* smooth noise array with the minimum of adjacent values
- * returns array one element shorter than noise
- * */
-function adjacent_min(noise) {
-    let out = [];
-    for(let i=0; i<noise.length; i++) {
-        out.push(min(noise[i],noise[i+1]));
-    }
-    return out;
-}
-
-function smoother(noise) {
-    let out = [];
-    for(let i=0; i<noise.length; i++) {
-        out.push((noise[i]+noise[i+1])/2);
-    }
-    return out;
-}
-function rougher(noise) {
-    let out = [];
-    for(let i=0; i<noise.length; i++) {
-        out.push((noise[i]-noise[i+1])/2);
-    }
-    return out;
-}
-
 /* write 1d noise to a bitmap, assumes noise is 100 samples, values from 0->1 */
 function noise1DToBitmap(noise,w,h, fname) {
     const img = PImage.make(w,h);
@@ -92,43 +98,12 @@ function noise1DToBitmap(noise,w,h, fname) {
     PImage.encodePNGToStream(img, fs.createWriteStream(fname)).then(() => console.log("wrote",fname));
 }
 
-
-
-function scaleFrom(value, oldmin, oldmax, newmin, newmax) {
-    let t =  (value-oldmin) / (oldmax-oldmin)
-    t = t*(newmax-newmin) + newmin;
-     return t;
-}
-
-function clampToBands(v,bands) {
-    return Math.round(v*bands)/bands;
-}
-
 function bandedNoiseTest() {
-    let gen = new SimplexNoise();
-    function snoise(nx, ny) {
-        // Rescale from -1.0:+1.0 to 0.0:1.0
-        return gen.noise2D(nx, ny) / 2 + 0.5;
-    }
-
-    function trigNoise(x,y) {
-        return scaleFrom(Math.cos(x/4.0)+Math.sin(y/4.0),-2,2,0,1)
-    }
-    let noise = [];
+    // let noise = [];
     const width = 200, height = 200;
-    for(let x=0; x<width; x++) {
-        for(let y = 0; y<height; y++) {
-            let i = x+y*width;
-            // noise[i]
-            //     = 1.0 * trigNoise(1*x,1*y)
-            //     + 0.5 * trigNoise(2*x,2*y)
-            // noise[i] = scaleFrom(noise[i],0,1,0,1);
-            var nx = x/width - 0.5, ny = y/height - 0.5;
-
-            noise[i] = (snoise(nx*2,ny*2)*0.5 + snoise(nx*4,ny*4)*0.25 + snoise(nx*8,ny*8)*0.125 + snoise(nx*16,ny*16)*(0.125/2));
-            noise[i] = clampToBands(noise[i],5);
-        }
-    }
+    let noise = gen_xy(width,height,(x,y,nx,ny)=>{
+        return clampToBands(snoise(nx*2,ny*2)*0.5 + snoise(nx*4,ny*4)*0.25 + snoise(nx*8,ny*8)*0.125 + snoise(nx*16,ny*16)*(0.125/2),5);
+    });
     noise1DToBitmap(noise,width,height,'band1.png');
 }
 bandedNoiseTest();
@@ -152,56 +127,33 @@ function rgb1DToBitmap(rgb,w,h,file_name) {
 
 
 function simpleGradientTest(){
-    let noise = [];
     const width = 200, height = 200;
-    for(let x=0; x<width; x++) {
-        for(let y = 0; y<height; y++) {
-            const i = x+y*width;
-            //get the center of each pixel
-            const nx = x/width - 0.5, ny = y/height - 0.5;
-            //generate noise with 4 octaves
-            noise[i] = (snoise(nx*2,ny*2)*0.5 + snoise(nx*4,ny*4)*0.25 + snoise(nx*8,ny*8)*0.125 + snoise(nx*16,ny*16)*(0.125/2));
-            // noise[i] = clampToBands(noise[i],5);
-        }
-    }
+    let noise = gen_xy(width,height,(x,y,nx,ny)=>{
+        //generate noise with 4 octaves
+        return (snoise(nx*2,ny*2)*0.5 + snoise(nx*4,ny*4)*0.25 + snoise(nx*8,ny*8)*0.125 + snoise(nx*16,ny*16)*(0.125/2));
+    });
 
-    function lerpRGB(t, A, B) {
-        return {
-            r: lerp(t, A.r, B.r),
-            g: lerp(t, A.g, B.g),
-            b: lerp(t, A.b, B.b)
-        }
-    }
-    const blue = {r:0, g:0, b:1};
-    const white = {r:1, g:1, b:1};
-    let rgb = noise.map((v)=>lerpRGB(v,white,blue));
+    //map to a simple white to blue gradient
+    let rgb = noise.map((v)=>lerpRGB(v,{r:1,g:1,b:1},{r:0,g:0,b:1}));
     rgb1DToBitmap(rgb,width,height,"grad1.png");
 }
 
 simpleGradientTest();
 
 function multiColorGradientTest() {
-    let noise = [];
     const width = 100, height = 100;
-    for(let x=0; x<width; x++) {
-        for(let y = 0; y<height; y++) {
-            const i = x+y*width;
-            //get the center of each pixel
-            const nx = x/width - 0.5, ny = y/height - 0.5;
-            //generate noise with 4 octaves
-            noise[i] = (snoise(nx*2,ny*2)*0.5 + snoise(nx*4,ny*4)*0.25 + snoise(nx*8,ny*8)*0.125 + snoise(nx*16,ny*16)*(0.125/2));
-            // noise[i] = clampToBands(noise[i],5);
-        }
-    }
 
+    let noise = gen_xy(width,height,(x,y,nx,ny) => {
+        //generate noise with 6 octaves
+        return (snoise(nx*1,ny*1)*1/2
+            + snoise(nx*2,ny*2)*1/4
+            + snoise(nx*4,ny*4)*1/8
+            + snoise(nx*8,ny*8)*1/16
+            + snoise(nx*16,ny*16)*1/32
+            + snoise(nx*32,ny*32)*1/64
+        );
+    });
 
-    function lerpRGBs(t, arr) {
-        return {
-            r: lerps(t, arr.map((C)=>C.r)),
-            g: lerps(t, arr.map((C)=>C.g)),
-            b: lerps(t, arr.map((C)=>C.b)),
-        }
-    }
     //black to red to yellow to white gradient
     let rgb = noise.map((v)=>lerpRGBs(v, [
         {r:0, g:0, b:0},
@@ -215,9 +167,3 @@ function multiColorGradientTest() {
 }
 multiColorGradientTest();
 
-function colorTests() {
-    for(let t = 0; t<=1.0; t+=0.05) {
-        lerps(t, [0,1,0,1]);
-    }
-}
-colorTests();
